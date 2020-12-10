4551a1f7f59f3da6dedafa18cdbe8135861f2cac
HaeMinJ <haemin001212@naver.com>
2020-12-11 03:10:37 +0900

    -Add API only for crawler

--- YamCrawler/crawler.py
+++ YamCrawler/crawler.py
@@ -1,5 +1,7 @@
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
+from urllib import request, parse
+import json
 
 with urlopen('https://search.shopping.naver.com/best100v2/detail.nhn?catId=50000155') as response:
     soup = BeautifulSoup(response, 'html.parser')
@@ -7,7 +9,8 @@ with urlopen('https://search.shopping.naver.com/best100v2/detail.nhn?catId=50000
     allData = []
     for title in soup.select("p.cont"):
         allData.append([])
-        allData[i].append(title.get_text())
+        name = title.get_text()[1:-1]
+        allData[i].append(name)
         i = i+1
     i = 0
     for price in soup.select("div.price span.num"):
@@ -20,3 +23,9 @@ with urlopen('https://search.shopping.naver.com/best100v2/detail.nhn?catId=50000
         allData[i].append(imgUrl)
         i = i+1
     print(allData)
+
+    headers = {'Content-Type': 'application/json; chearset=utf-8'}
+    data = {'title': 'dummy title', 'id': 1, 'message': 'hello world!'}
+    req = request.Request('http://127.0.0.1:5000', headers=headers, data=json.dumps(data).encode('utf-8'))
+    res = request.urlopen(req)
+    print(str(res.status_code) + " | " + res.text)
