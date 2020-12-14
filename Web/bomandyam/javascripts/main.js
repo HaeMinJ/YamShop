d0ddf3a3bc9e80a9e77340e898ba41696785ae73
HaeMinJ <haemin001212@naver.com>
2020-12-15 02:45:25 +0900

    -Change Directory name for ejs engine

---
+++ Web/bomandyam/javascripts/main.js
@@ -0,0 +1,38 @@
+(function(){
+    let current = 0;
+    let max = 0;
+    let container;
+    let interval;
+
+    function init (){
+        container = jQuery(".slide ul");
+        max = container.children().length;
+        events();
+
+        interval = setInterval(next, 3000);
+    }
+    function events(){
+        jQuery("button.prev").on("click", prev);
+        jQuery("button.next").on("click", next);
+    }
+
+    function prev ( e ){
+        current--;
+        if( current<0 ) current = max-1;
+        animate();
+    }
+    function next( e ){
+        current++;
+        if(current>max-1) current = 0;
+        animate();
+    }
+    function animate(){
+        const moveX = current * 1200;
+        TweenMax.to( container, 0.8, { marginLeft: -moveX, ease: Expo.easeOut });
+
+        clearInterval(interval);
+        interval = setInterval(next, 3000);
+    }
+    jQuery( document ).ready( init );
+    // window.onload = init;
+})();
