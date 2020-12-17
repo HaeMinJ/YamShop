9f4665b1264dea0ee925465639c8d69b709aa23f
LAPTOP-P9Q4AJ5V\ghtjd <ghtjd626@naver.com>
2020-12-18 01:38:29 +0900

    관리자 삭제랑 페이징

--- YamAdmin/routes/pick.js
+++ YamAdmin/routes/pick.js
@@ -2,12 +2,11 @@ let express = require('express');
 let router = express.Router();
 
 /* GET home page. */
-router.get('/', async function(req, res, next) {
-
-    let result = await fetch('https://api.bomandyam.shop:3000/product?page=0&size=30')
+router.get('/bomspick', async function(req, res, next) {
+    let result = await fetch('https://api.bomandyam.shop:3000/product?page=1&size=15')
     let products = await result.json()
     console.log(products)
-    res.render('pages/yamspick', { products: products });
+    res.render('pages/bomspick', { products: products });
 });
 
 module.exports = router;
