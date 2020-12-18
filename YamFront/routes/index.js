f700b75ff79b95403982eb8c66040b0d207c80e2
HaeMinJ <haemin001212@naver.com>
2020-12-18 22:31:42 +0900

    error fixed

--- YamFront/routes/index.js
+++ YamFront/routes/index.js
@@ -14,8 +14,8 @@ router.get('/', async function(req, res, next) {
     catbests = await fetch('https://api.bomandyam.shop:3000/product/catbest')
     catbests = await catbests.json()
 
-    recommends = await fetch('https://api.bomandyam.shop:3000/product/recommend')
-    recommends = await categories.json()
+    // recommends = await fetch('https://api.bomandyam.shop:3000/product/recommend')
+    // recommends = await recommends.json()
   }catch (e) {
     console.log(e);
   }
